e3fce5a92e73c94f4a85bc6f3227997fd2dc4615
薛丞宏 <ihcaoe@gmail.com>
2017-08-18 16:49:42 +0800

    加設定

--- 語料庫/admin.py
+++ 語料庫/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+# from django.contrib.sites.models import Site
+from 語料庫.models import 語料表
+from 語料庫.models import 音檔表
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+# admin.site.unregister(Site)
+
+admin.site.disable_action('delete_selected')
+
+
+class 語料表管理(admin.ModelAdmin):
+    list_display = ['id', '類別', '漢字', '書寫', '對齊狀態', ]
+    ordering = ['-id']
+    list_filter = ['音檔__類別', '漢字', ]
+
+    actions = [
+        '設定類別_教材',
+    ]
+
+    def 設定類別_教材(self, request, queryset):
+        queryset.update(類別='S1')
+
+
+admin.site.register(音檔表)
+admin.site.register(語料表, 語料表管理)
