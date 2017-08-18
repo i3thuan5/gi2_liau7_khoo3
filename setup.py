3989c9d6207f47becf7a96d6568dc24b7e99deac
薛丞宏 <ihcaoe@gmail.com>
2017-08-18 15:29:56 +0800

    加試驗設定檔

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='語料庫整理',
+    packages=[],
+    version='0.1.0',
+    author='薛丞宏',
+    author_email='ihcaoe@gmail.com',
+    url='https://xn--v0qr21b.xn--kpry57d/',
+    keywords=[
+        'Corpus', '語料庫',
+        'Taiwan', 'Taiwanese',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=[
+        'django',
+    ],
+)
