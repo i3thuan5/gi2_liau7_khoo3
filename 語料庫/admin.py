493e57303d24668c232e4a5e4d35633a6f3a7bf4
a8568730 <a8568730@hotmail.com>
2017-08-22 14:30:41 +0800

    + 語料表新增頁的模板

--- 語料庫/admin.py
+++ 語料庫/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User, Group
 # from django.contrib.sites.models import Site
 from 語料庫.models import 語料表
 from 語料庫.models import 音檔表
+from django.template.response import TemplateResponse
+from django.conf.urls import url
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
@@ -41,6 +43,21 @@ class 語料表管理(admin.ModelAdmin):
     def 設定類別_教材(self, request, queryset):
         queryset.update(類別='S1')
 
-
+    change_form_template = 'admin/gi2_liau7_khoo3/語料表/custom_change_form.html'
+    def get_urls(self):
+        urls = super(語料表管理, self).get_urls()
+        my_urls = [
+            url(r'^語料表/$', self.admin_site.admin_view(self.my_view))
+        ]
+        return my_urls + urls
+    def my_view(self, request):
+        # ...
+        context = dict(
+           # Include common variables for rendering the admin template.
+           self.admin_site.each_context(request),
+           # Anything else you want in the context...
+        #            key=value,
+        )
+        return TemplateResponse(request, 'admin/gi2_liau7_khoo3/語料表/custom_change_form.html', context)
 admin.site.register(音檔表)
 admin.site.register(語料表, 語料表管理)
