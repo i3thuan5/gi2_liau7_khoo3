8897cfb2f85491513d7871cd2e5e1a32cd36d331
薛丞宏 <ihcaoe@gmail.com>
2017-08-25 19:52:02 +0800

    佇程式開始的時陣，檢查有normalize-audio通用無

--- 語料庫/apps.py
+++ 語料庫/apps.py
@@ -1,5 +1,18 @@
+from os.path import join
+from tempfile import TemporaryDirectory
+
 from django.apps import AppConfig
+from 臺灣言語工具.語音辨識.聲音檔 import 聲音檔
+from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
 
 
 class 語料庫Config(AppConfig):
     name = '語料庫'
+
+    def ready(self):
+        with TemporaryDirectory() as 資料夾:
+            檔名 = join(資料夾, 'audio.wav')
+            音檔 = 聲音檔.對參數轉(2, 8000, 1, b'sui2')
+            with open(檔名, 'wb') as 檔案:
+                檔案.write(音檔.wav格式資料())
+            程式腳本._走指令(['normalize-audio', 檔名])
