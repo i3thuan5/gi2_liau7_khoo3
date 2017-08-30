4cb7d48ff3f3b737554557e033172814eed2dfe2
薛丞宏 <ihcaoe@gmail.com>
2017-08-30 18:17:25 +0800

    佮33討論頭一擺愛標啥label #34 #43

--- 語料庫/models.py
+++ 語料庫/models.py
@@ -84,4 +84,4 @@ class 語料狀況表(models.Model):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.狀況
+        return '{} {}'.format(self.pk, self.狀況.split('：')[-1])
