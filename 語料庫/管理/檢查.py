8f16275692849b3fe548c0c2bec29a363c6a5ed3
a8568730 <a8568730@hotmail.com>
2017-09-05 17:18:48 +0800

    檢查表changelist顯示播放音檔

--- 語料庫/管理/檢查.py
+++ 語料庫/管理/檢查.py
@@ -4,6 +4,7 @@ from 語料庫.管理.校對 import 校對表管理
 
 
 class 檢查表(語料表):
+
     class Meta:
         proxy = True
         verbose_name = "2.檢查表"
@@ -11,8 +12,25 @@ class 檢查表(語料表):
 
 
 class 檢查表管理(校對表管理):
-    readonly_fields = ('音檔', '漢字', '本調臺羅', '口語調臺羅', )
+    # change list
+    list_display = ['id', '目標音檔', '漢字', '本調臺羅', '口語調臺羅', '校對者', '校對時間']
+    actions = [
+        '設定無問題',
+    ]
 
+    def 目標音檔(self, obj):
+        return ('''<audio controls>
+        <source src='/音檔/{}/{}/{}/audio.wav'>
+        Your browser does not support the audio element.</audio>'''.format(
+            obj.音檔.id, obj.聲音開始時間, obj.聲音結束時間
+        ))
+    目標音檔.allow_tags = True
+
+    def 設定無問題(self, request, queryset):
+        queryset.update(檢查者=request.user)
+
+    # change view
+    readonly_fields = ('音檔', '漢字', '本調臺羅', '口語調臺羅', )
     fieldsets = (
         ('音檔', {
             'fields': ('音檔', ),
@@ -27,13 +45,6 @@ class 檢查表管理(校對表管理):
             'classes': ['wide']
         }),
     )
-
-    actions = [
-        '設定無問題',
-    ]
-
-    def 設定無問題(self, request, queryset):
-        queryset.update(檢查者=request.user)
 
     def save_model(self, request, obj, form, change):
         # 儲存校對者
