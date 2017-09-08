becb485c1294f66d016f240bcc15519b6559682b
a8568730 <a8568730@hotmail.com>
2017-09-08 17:58:09 +0800

    顯示今天的校對數量和日期

--- 語料庫/templatetags/gi2_liau7_tags.py
+++ 語料庫/templatetags/gi2_liau7_tags.py
@@ -1,10 +1,11 @@
 from django import template
 from 語料庫.models import 語料表
+from django.utils.datetime_safe import date
 
 register = template.Library()
 
 
 @register.simple_tag
 def 今仔日校對數量():
-    數量 = 語料表.objects.get(pk=1).count()
+    數量 = 語料表.objects.filter(校對時間__date=date.today()).count()
     return 數量 
