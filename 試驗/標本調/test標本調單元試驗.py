ead9e838e9cfec249488c9f25ad94a895a6e66b6
fafoy <ihcaoe@gmail.com>
2017-09-19 22:27:12 +0800

    無平長就照漢字 #94

--- 試驗/標本調/test標本調單元試驗.py
+++ 試驗/標本調/test標本調單元試驗.py
@@ -34,3 +34,8 @@ class 標本調試驗(TestCase):
         self.漢字 = '（查某）人'
         self.原本本調 = 'tsa9-lang5'
         self.答案本調 = 'tsa9 lang5'
+
+    def test字數無仝照漢字(self):
+        self.漢字 = '火車已經到'
+        self.原本本調 = 'hue2-tshia1'
+        self.答案本調 = 'hue2-tshia1 i2-king1 kau3'
