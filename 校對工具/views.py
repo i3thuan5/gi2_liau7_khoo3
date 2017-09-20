3e5f5f5e7aa5c944e91bbe61cca953bcbb463827
fafoy <ihcaoe@gmail.com>
2017-09-20 21:28:20 +0800

    訓練語言模型

--- 校對工具/views.py
+++ 校對工具/views.py
@@ -4,12 +4,17 @@ from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 im
 from 用字.models import 用字表
 from 臺灣言語工具.基本物件.公用變數 import 無音
 from 臺灣言語工具.斷詞.拄好長度辭典揣詞 import 拄好長度辭典揣詞
-# from 臺灣言語工具.斷詞.語言模型揀集內組 import 語言模型揀集內組
-from 臺灣言語工具.解析整理.座標揀集內組 import 座標揀集內組
+from 臺灣言語工具.斷詞.語言模型揀集內組 import 語言模型揀集內組
+# from 臺灣言語工具.解析整理.座標揀集內組 import 座標揀集內組
 from 臺灣言語工具.辭典.型音辭典 import 型音辭典
 from 校對工具.models import 辭典表
 from 臺灣言語工具 import 解析整理
+from os.path import join
 import re
+from tempfile import TemporaryDirectory
+from 語言模型.models import 語言模型表
+from 臺灣言語工具.語言模型.KenLM語言模型訓練 import KenLM語言模型訓練
+from 臺灣言語工具.語言模型.KenLM語言模型 import KenLM語言模型
 
 
 class 工具:
@@ -19,6 +24,18 @@ class 工具:
         for 分詞 in 辭典表.全部分詞():
             self.辭典.加詞(
                 拆文分析器.分詞詞物件(分詞)
+            )
+        with TemporaryDirectory() as 資料夾:
+            語句 = join(資料夾, '語句.txt')
+            with open(語句, 'w') as 檔案:
+                for 分詞 in 語言模型表.全部分詞():
+                    print(分詞, file=檔案)
+            self.語言模型 = KenLM語言模型(
+                KenLM語言模型訓練().訓練(
+                    [語句],
+                    join(資料夾, '暫存資料夾'),
+                    連紲詞長度=3
+                )
             )
 
     def 標本調(self, 漢字, 原本臺羅):
@@ -39,8 +56,8 @@ class 工具:
         結果句物件 = (
             原本句物件
             .揣詞(拄好長度辭典揣詞, self.辭典)
-            .揀(座標揀集內組)
-            #             .揀(語言模型揀集內組, self.語言模型)
+            #             .揀(座標揀集內組)
+            .揀(語言模型揀集內組, self.語言模型)
         )
         for 字物件 in 結果句物件.篩出字物件():
             if 字物件.音 == 無音:
