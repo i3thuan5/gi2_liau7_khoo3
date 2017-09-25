5f907ba50d3432f792008403885d0e434dd3842c
薛丞宏 <ihcaoe@gmail.com>
2017-09-25 21:58:58 +0800

    加匯校對語料

--- 語言模型/management/commands/匯入教典例句.py
+++ 語言模型/management/commands/匯入教典例句.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from 語言模型.匯資料 import 匯入教典例句
+from 語言模型.匯校對 import 匯入校對語料
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **參數):
         匯入教典例句()
+        匯入校對語料()
