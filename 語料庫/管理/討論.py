1c00b11a26967b8dc4bb6be34d2dd5f6557b1f2c
a8568730 <a8568730@hotmail.com>
2017-09-27 12:02:43 +0800

    討論表繼承校對表

--- 語料庫/管理/討論.py
+++ 語料庫/管理/討論.py
@@ -1,19 +1,13 @@
-from 語料庫.models import 語料狀況表
-from 語料庫.models import 語料表
 from 語料庫.管理.校對 import 校對表管理
-from django.utils.timezone import now
+from 語料庫.管理.校對 import 校對表
 
 
-class 討論表(語料表):
+class 討論表(校對表):
 
     class Meta:
         proxy = True
         verbose_name = "討論表"
         verbose_name_plural = verbose_name
-
-    def save(self, *args, **kwargs):
-        self.檢查時間 = now()
-        super().save(*args, **kwargs)
 
 
 class 討論表管理(校對表管理):
@@ -23,14 +17,8 @@ class 討論表管理(校對表管理):
         '目標音檔', '狀況',
         '漢字', '本調臺羅', '口語調臺羅',
         '備註開頭',
-        '檢查時間',
+        '校對者', '校對時間'
     ]
-    ordering = ['檢查者', 'id']
-    list_per_page = 10
-    actions = [
-        '設定無問題',
-    ]
-    list_filter = ['音檔__類別', '語料狀況', '校對者', '檢查者', ]
 
     def 目標音檔(self, obj):
         return ('''<audio controls>
@@ -39,27 +27,6 @@ class 討論表管理(校對表管理):
             obj.音檔.id, obj.聲音開始時間, obj.聲音結束時間
         ))
     目標音檔.allow_tags = True
-
-    def 設定無問題(self, request, queryset):
-        queryset.update(檢查者=request.user, 檢查時間=now())
-
-    # change view
-    change_form_template = 'admin/gi2_liau7_khoo3/語料表/custom_change_form.html'
-    readonly_fields = ('音檔', '漢字', '本調臺羅', '口語調臺羅', '語料狀況',)
-    fieldsets = (
-        ('音檔', {
-            'fields': ('音檔', ),
-            'classes': ['wide']
-        }),
-        (None, {
-            'fields': ('漢字', '本調臺羅', '口語調臺羅', '備註', ),
-            'classes': ['wide']
-        }),
-        (None, {
-            'fields': ('語料狀況', ),
-            'classes': ['wide']
-        }),
-    )
 
     def get_queryset(self, request):
         qs = super(校對表管理, self).get_queryset(request)
