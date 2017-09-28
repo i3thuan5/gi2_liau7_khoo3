64ad91d574348562548e1ce0640bab937e9e9577
a8568730 <a8568730@hotmail.com>
2017-09-28 10:22:06 +0800

    繼承目標音檔欄

--- 語料庫/管理/Kaldi篩掉.py
+++ 語料庫/管理/Kaldi篩掉.py
@@ -2,6 +2,7 @@
 from 語料庫.models import 語料表
 from 語料庫.管理.校對 import 校對表管理
 from django.utils.timezone import now
+from 語料庫.widgets.目標音檔欄 import 目標音檔欄
 # from 語料庫.管理.檢查 import 檢查表管理
 
 
@@ -13,11 +14,10 @@ class Kaldi篩掉表(語料表):
         verbose_name_plural = verbose_name
 
     def save(self, *args, **kwargs):
-        self.檢查時間 = now()
         super().save(*args, **kwargs)
 
 
-class Kaldi篩掉表管理(校對表管理):
+class Kaldi篩掉表管理(校對表管理, 目標音檔欄):
     # change list
     list_display = [
         'id',
@@ -30,14 +30,6 @@ class Kaldi篩掉表管理(校對表管理):
     actions = []
     list_filter = ()
     list_display_links = None
-
-    def 目標音檔(self, obj):
-        return ('''<audio controls>
-        <source src='/音檔/{}/{}/{}/audio.wav'>
-        Your browser does not support the audio element.</audio>'''.format(
-            obj.音檔.id, obj.聲音開始時間, obj.聲音結束時間
-        ))
-    目標音檔.allow_tags = True
 
     def get_queryset(self, request):
         qs = super(校對表管理, self).get_queryset(request)
