6c91c3bf4c77b1e54fa58b10ac0b0c6ac901ee88
a8568730 <a8568730@hotmail.com>
2017-09-30 11:30:15 +0800

    reformat

--- gi2_liau7_khoo3/settings/base.py
+++ gi2_liau7_khoo3/settings/base.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5hf5)s^_b=m4=cg=_jhpe30mjks1l-v%n3y9b$w(ll5d$hh-ts' 
+SECRET_KEY = '5hf5)s^_b=m4=cg=_jhpe30mjks1l-v%n3y9b$w(ll5d$hh-ts'
 
 ALLOWED_HOSTS = ['*']
 
