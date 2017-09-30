9d213baec0d565d88ae4b19275b29cc632e388e3
a8568730 <a8568730@hotmail.com>
2017-09-30 12:26:50 +0800

    修正STATIC_ROOT

--- gi2_liau7_khoo3/settings/base.py
+++ gi2_liau7_khoo3/settings/base.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticAll')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
 
