0cd1a042e18583394cf424e7ee674c09d8b5460e
a8568730 <a8568730@hotmail.com>
2017-10-04 17:45:21 +0800

    allow_tags deprecated, 改用format_html()

--- 語料庫/widgets/目標音檔欄.py
+++ 語料庫/widgets/目標音檔欄.py
@@ -1,10 +1,11 @@
+from django.utils.html import format_html
+
 
 class 目標音檔欄():
 
     def 目標音檔(self, obj):
-        return ('''<audio controls>
+        return format_html('''<audio controls>
         <source src='/音檔/{}/{}/{}/audio.wav'>
-        Your browser does not support the audio element.</audio>'''.format(
-            obj.音檔.id, obj.聲音開始時間, obj.聲音結束時間
-        ))
-    目標音檔.allow_tags = True
+        Your browser does not support the audio element.</audio>''',
+                           obj.音檔.id, obj.聲音開始時間, obj.聲音結束時間
+                           )
