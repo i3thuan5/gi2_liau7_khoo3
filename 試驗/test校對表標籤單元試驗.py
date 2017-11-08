03c44d8f28bd031612de9cf945b377aa90624da5
a8568730 <a8568730@hotmail.com>
2017-11-08 11:18:13 +0800

    新增剩餘校對數量tag

--- 試驗/test校對表標籤單元試驗.py
+++ 試驗/test校對表標籤單元試驗.py
@@ -8,35 +8,40 @@ from django.contrib.auth.models import User
 
 class 校對表標籤試驗(TestCase):
 
-    def setUp(self):
+    def 新增音檔(self, 類別, 資料夾名, 聲音檔名, 聽拍檔名):
         一音檔 = 音檔表.objects.create(
-            類別='戲劇',
-            資料夾名='dirsui2',
-            聲音檔名='sui2.wav',
-            聽拍檔名='sui2.txt',
+            類別=類別,
+            資料夾名=資料夾名,
+            聲音檔名=聲音檔名,
+            聽拍檔名=聽拍檔名,
         )
-        一助理 = User.objects.create_user('小豬', 'a@gmail.com', '3333')
-        校對表.objects.create(
-            校對者=一助理,
+        return 一音檔
 
-            音檔=一音檔,
+    def 新增助理(self, 名, 信箱, 密碼):
+        一助理 = User.objects.create_user(名, 信箱, 密碼)
+        return 一助理
+
+    def 新增語料(self, 校對者, 音檔, 有揀出來用無):
+        校對表.objects.create(
+            校對者=校對者,
+            音檔=音檔,
             聲音結束時間='0',
             聲音開始時間='1',
-
-            語者='sui2',
-            頭一版資料='sui2',
-            頭一版通用='sui',
             漢字='媠',
             本調臺羅='sui2',
             口語調臺羅='sui2',
-
             sing5hong5舊編號='333',
             sing5hong5新編號='333',
-            sing5hong5有揀出來用無=True,
+            sing5hong5有揀出來用無=有揀出來用無,
             愛先做無=True,
         )
 
+    def setUp(self):
+        self.一音檔 = self.新增音檔('戲劇', 'dirsui2', 'sui2.wav', 'sui2.txt',)
+        self.一助理 = self.新增助理('小豬', 'a@gmail.com', '3333')
+
     def test今仔日校對數量(self):
+        self.新增語料(self.一助理, self.一音檔, True)
         rendered = Template(
             '{% load 今仔日校對數量 from gi2_liau7_tags %}'
             '{% 今仔日校對數量 %}'
@@ -44,8 +49,37 @@ class 校對表標籤試驗(TestCase):
         self.assertEqual(rendered, "1")
 
     def test攏總校對數量(self):
+        self.新增語料(self.一助理, self.一音檔, True)
         rendered = Template(
             '{% load 攏總校對數量 from gi2_liau7_tags %}'
             '{% 攏總校對數量 %}'
         ).render(Context())
         self.assertEqual(rendered, "1")
+
+    def test賰校對數量(self):
+        self.新增語料(None, self.一音檔, True)
+        self.新增語料(self.一助理, self.一音檔, True)
+        rendered = Template(
+            '{% load 賰校對數量 from gi2_liau7_tags %}'
+            '{% 賰校對數量 %}'
+        ).render(Context())
+        self.assertEqual(rendered, "1")
+        
+    def test賰校對數量_校對完畢(self):
+        self.新增語料(self.一助理, self.一音檔, True)
+        self.新增語料(self.一助理, self.一音檔, True)
+        rendered = Template(
+            '{% load 賰校對數量 from gi2_liau7_tags %}'
+            '{% 賰校對數量 %}'
+        ).render(Context())
+        self.assertEqual(rendered, "0")
+
+    def test賰校對數量_扣掉沒挑出來用的(self):
+        self.新增語料(None, self.一音檔, True)
+        self.新增語料(None, self.一音檔, True)
+        self.新增語料(None, self.一音檔, False)
+        rendered = Template(
+            '{% load 賰校對數量 from gi2_liau7_tags %}'
+            '{% 賰校對數量 %}'
+        ).render(Context())
+        self.assertEqual(rendered, "2")
--- 語料庫/templatetags/gi2_liau7_tags.py
+++ 語料庫/templatetags/gi2_liau7_tags.py
@@ -13,5 +13,11 @@ def 今仔日校對數量():
 
 @register.simple_tag
 def 攏總校對數量():
-    數量 = 語料表.objects.filter(校對者__isnull=False, 校對時間__isnull=False).count()
+    數量 = 語料表.objects.filter(校對者__isnull=False).count()
+    return 數量
+
+
+@register.simple_tag
+def 賰校對數量():
+    數量 = 語料表.objects.filter(校對者__isnull=True, sing5hong5有揀出來用無=True).count()
     return 數量
