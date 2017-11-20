6619bfb69310c662ac64ca227cce0683ffd7036b
a8568730 <a8568730@hotmail.com>
2017-11-20 13:33:39 +0800

    將signals改放在signals.py

--- 語料庫/apps.py
+++ 語料庫/apps.py
@@ -4,8 +4,6 @@ from tempfile import TemporaryDirectory
 from django.apps import AppConfig
 from 臺灣言語工具.語音辨識.聲音檔 import 聲音檔
 from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
-from django.db.models import signals
-from 語料庫.signals.新增對齊狀態 import 新增對齊狀態
 
 
 class 語料庫Config(AppConfig):
@@ -28,11 +26,5 @@ class 語料庫Config(AppConfig):
                 程式腳本._走指令(['sox', 檔名, '-t', 'wav', '/dev/null', 'remix', '1'],)
             except RuntimeError:
                 raise OSError('請裝sox!! `sudo apt-get install sox`')
-        
-        # importing model classes
-        from .models import 語料表
-        語料表 = self.get_model('語料表')
-
         # registering signals
-        signals.post_save.connect(新增對齊狀態, sender='語料庫.語料表', weak=False,
-                                  dispatch_uid='models.新增對齊狀態')
+        import 語料庫.signals
--- 語料庫/signals.py
+++ 語料庫/signals.py
@@ -5,6 +5,7 @@ from 校對工具.檢查本調拼音 import 判斷本調拼音
 @receiver(post_save, sender='語料庫.語料表')
 def 新增對齊狀態(sender, instance, **kwargs):
     """create 對齊狀態 for every new 語料."""
+    print("""create 對齊狀態 for every new 語料.""")
     print('instance=', instance)
 #     if getattr(instance, '漢字', True) and getattr(instance, '本調臺羅', True):
 #         狀態字串 = 判斷本調拼音(instance.漢字, instance.本調臺羅)
@@ -21,6 +22,3 @@ def 新增對齊狀態(sender, instance, **kwargs):
 # #                 self.對齊狀態 = 新狀態
 # #             self.對齊狀態.save()
 #             對齊狀態表.objects.create(thing=instance)
-
-# signals.post_save.connect(新增對齊狀態, sender=語料表, weak=False,
-#     dispatch_uid='models.新增對齊狀態')
