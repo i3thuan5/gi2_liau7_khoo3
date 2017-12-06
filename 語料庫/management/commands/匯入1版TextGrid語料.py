6b1196dd2937883124f4af53729e582a4e7c3458
薛丞宏 <ihcaoe@gmail.com>
2017-12-06 15:11:40 +0800

    調整參數

--- 語料庫/management/commands/匯入1版TextGrid語料.py
+++ 語料庫/management/commands/匯入1版TextGrid語料.py
@@ -34,7 +34,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'json資料夾所在', type=str
+            'textgrid資料夾所在', type=str
         )
         parser.add_argument(
             'wav資料夾所在', type=str
@@ -48,41 +48,40 @@ class Command(BaseCommand):
 
     @transaction.atomic()
     def handle(self, *args, **參數):
-        for json檔名, wav檔名 in 揣資料夾內的語料(參數['json資料夾所在'], 參數['wav資料夾所在']):
-            if (
-                basename(dirname(json檔名)) == 參數['資料夾名'] and
-                basename(json檔名) == 參數['聽拍檔名']
-            ):
-                類別 = self.語料類別[參數['資料夾名']]
-                資料夾名 = basename(dirname(wav檔名))
-                聲音檔名 = basename(wav檔名)
-                聽拍檔名 = basename(json檔名)
-                音檔資料 = 音檔表.objects.create(
-                    類別=類別,
-                    資料夾名=資料夾名,
-                    聲音檔名=聲音檔名,
-                    聽拍檔名=聽拍檔名,
+        textgird檔名 = join(參數['textgrid資料夾所在'], 參數['資料夾名'], 參數['聽拍檔名'])
+        wav檔名 = join(參數['wav資料夾所在'], 參數['資料夾名'],
+                     參數['聽拍檔名'].split('|')[0].replace('&amp;', '&'))
+        類別 = self.語料類別[參數['資料夾名']]
+        資料夾名 = basename(dirname(wav檔名))
+        聲音檔名 = basename(wav檔名)
+        聽拍檔名 = basename(textgird檔名)
+        音檔資料 = 音檔表.objects.create(
+            類別=類別,
+            資料夾名=資料夾名,
+            聲音檔名=聲音檔名,
+            聽拍檔名=聽拍檔名,
+        )
+        return
+        with open(json檔名) as json檔案:
+            for 一句 in json.load(json檔案):
+                音檔資料.資料.create(
+                    聲音結束時間=一句["結束時間"],
+                    聲音開始時間=一句["開始時間"],
+
+                    語者=一句["語者"],
+                    頭一版資料=一句["trs聽拍"],
+                    頭一版通用=一句["原始通用"],
+                    漢字=一句["漢字"],
+                    本調臺羅=一句["臺羅"],
+                    口語調臺羅=一句["口語調臺羅"],
+
+                    sing5hong5舊編號=一句["舊編號"],
+                    sing5hong5新編號=一句["新編號"],
+                    sing5hong5有揀出來用無=一句["有揀出來無"],
+                    愛先做無=self.判斷先愛先做無(一句["漢字"]),
                 )
-                with open(json檔名) as json檔案:
-                    for 一句 in json.load(json檔案):
-                        音檔資料.資料.create(
-                            聲音結束時間=一句["結束時間"],
-                            聲音開始時間=一句["開始時間"],
-
-                            語者=一句["語者"],
-                            頭一版資料=一句["trs聽拍"],
-                            頭一版通用=一句["原始通用"],
-                            漢字=一句["漢字"],
-                            本調臺羅=一句["臺羅"],
-                            口語調臺羅=一句["口語調臺羅"],
-
-                            sing5hong5舊編號=一句["舊編號"],
-                            sing5hong5新編號=一句["新編號"],
-                            sing5hong5有揀出來用無=一句["有揀出來無"],
-                            愛先做無=self.判斷先愛先做無(一句["漢字"]),
-                        )
-                with open(wav檔名, 'rb') as 檔案:
-                    音檔資料.原始檔.save(資料夾名 + '/' + 聲音檔名, File(檔案))
+        with open(wav檔名, 'rb') as 檔案:
+            音檔資料.原始檔.save(資料夾名 + '/' + 聲音檔名, File(檔案))
 
     @classmethod
     def 判斷先愛先做無(cls, 語句):
