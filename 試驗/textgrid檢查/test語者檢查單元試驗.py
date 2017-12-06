e27e22d1435bf233856ee9d9f2e874e127296717
薛丞宏 <ihcaoe@gmail.com>
2017-12-06 17:02:03 +0800

    語者檢查了矣

--- 試驗/textgrid檢查/test語者檢查單元試驗.py
+++ 試驗/textgrid檢查/test語者檢查單元試驗.py
@@ -1,17 +1,23 @@
-from os.path import join
-from tempfile import TemporaryDirectory
 
-from django.contrib.auth.models import User
-from django.core.management import call_command
 from django.test.testcases import TestCase
-
-
-from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 語言模型.models import 語言模型表
-from 校對工具.views import 工具
-from 語料庫.models import 音檔表
+from 語料庫.praat檢查 import praat檢查
 
 
 class textgrid試驗(TestCase):
+
+    def setUp(self):
+        self.檢查 = praat檢查()
+
+    def test_語者時間正常(self):
+        self.檢查.檢查語者(
+            [('0', '10', 'tsit8'), ('10', '20', 'nng7'), ('20', '30', 'sann1')],
+            [('0', '20', '麗芸'), ('20', '30', '添福')],
+        )
+        self.assertEqual(len(self.檢查.錯誤), 0)
+
     def test_語者時間無對著(self):
-        self.fail()
+        self.檢查.檢查語者(
+            [('0', '10', 'tsit8'), ('10', '20', 'nng7'), ('20', '30', 'sann1')],
+            [('0', '20.3', '麗芸'), ('20.3', '30', '添福')],
+        )
+        self.assertEqual(len(self.檢查.錯誤), 1)
