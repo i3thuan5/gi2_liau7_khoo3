444bb7cfbc980e79410d0d54444e5f9300dcf715
薛丞宏 <ihcaoe@gmail.com>
2017-12-06 17:21:56 +0800

    重排版

--- 試驗/textgrid檢查/test檔案檢查單元試驗.py
+++ 試驗/textgrid檢查/test檔案檢查單元試驗.py
@@ -19,7 +19,7 @@ class textgrid試驗(TestCase):
     @patch('語料庫.praat檢查.praat檢查.檢查聽拍')
     @patch('語料庫.praat檢查.praat檢查.提出聽拍資料')
     def test_無speech資料就攏免檢查(self, 提出聽拍資料mock, 檢查聽拍mock, 檢查語者mock):
-        提出聽拍資料mock.return_value=[]
+        提出聽拍資料mock.return_value = []
         提出聽拍資料mock.side_effect = KeyError
         self.檢查.檢查檔案('MH.TextGrid')
         檢查聽拍mock.assert_not_called()
@@ -37,18 +37,19 @@ class textgrid試驗(TestCase):
     @patch('語料庫.praat檢查.praat檢查.提出語者資料')
     @patch('語料庫.praat檢查.praat檢查.提出聽拍資料')
     def test_無turns就免檢查語者時間(self, 提出聽拍資料mock, 提出語者資料mock, 檢查聽拍mock, 檢查語者mock):
-        提出聽拍資料mock.return_value=[]
+        提出聽拍資料mock.return_value = []
         提出語者資料mock.side_effect = KeyError
         self.檢查.檢查檔案('MH.TextGrid')
         檢查聽拍mock.assert_called_once_with([])
         檢查語者mock.assert_not_called()
+
     @patch('語料庫.praat檢查.praat檢查.檢查語者')
     @patch('語料庫.praat檢查.praat檢查.檢查聽拍')
     @patch('語料庫.praat檢查.praat檢查.提出語者資料')
     @patch('語料庫.praat檢查.praat檢查.提出聽拍資料')
     def test_攏正常(self, 提出聽拍資料mock, 提出語者資料mock, 檢查聽拍mock, 檢查語者mock):
-        提出聽拍資料mock.return_value=[]
-        提出語者資料mock.return_value=[]
+        提出聽拍資料mock.return_value = []
+        提出語者資料mock.return_value = []
         self.檢查.檢查檔案('MH.TextGrid')
         檢查聽拍mock.assert_called_once_with([])
-        檢查語者mock.assert_called_once_with([],[])
+        檢查語者mock.assert_called_once_with([], [])
