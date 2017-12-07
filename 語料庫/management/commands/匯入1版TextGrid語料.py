a64851bd0f10ac917ca2c41b0336ac8469379897
薛丞宏 <ihcaoe@gmail.com>
2017-12-07 10:56:24 +0800

    有法度檢查錯誤資訊矣

--- 語料庫/management/commands/匯入1版TextGrid語料.py
+++ 語料庫/management/commands/匯入1版TextGrid語料.py
@@ -9,6 +9,7 @@ from praatio.tgio import openTextgrid
 
 from 語料庫.models import 音檔表
 from 語料庫.management.commands.匯入1版trs語料 import Command as trs指令
+from 語料庫.praat檢查 import praat檢查
 
 
 class Command(trs指令):
@@ -38,36 +39,35 @@ class Command(trs指令):
             聲音檔名=basename(wav檔名),
             聽拍檔名=basename(textgird檔名),
         )
-        tg = openTextgrid(textgird檔名)
-
-        print(tg.tierNameList)
-        # Get all intervals
-        entryList = tg.tierDict["Speech"].entryList[:10]
-        print(entryList)
-        a,b,c=entryList[0]
-        print(a,b,c)
-
-        entryList = tg.tierDict["Turns"].find("添福")[:10]  # Get all instances of 'a'
-        print(entryList)
-        print( tg.tierDict["Turns"].entryList[41])
-        
-        raise NotImplementedError()
-        for 一句 in json.load(json檔案):
+        檢查 = praat檢查()
+        檢查.檢查檔案(textgird檔名)
+        if 檢查.有錯誤無():
+            print(檢查.錯誤資訊())
+            raise NotImplementedError()
+            return
+        語者資料 = self.提出語者資料(textgird檔名)
+        for 開始, 結束, 聽拍 in self.提出聽拍資料(textgird檔名):
+            
             音檔資料.資料.create(
-                聲音結束時間=一句["結束時間"],
-                聲音開始時間=一句["開始時間"],
+                聲音結束時間=開始,
+                聲音開始時間=結束,
 
-                語者=一句["語者"],
-                頭一版資料=一句["trs聽拍"],
+                語者=self.揣語者(語者資料,開始),
+                頭一版資料=聽拍,
                 頭一版通用=一句["原始通用"],
                 漢字=一句["漢字"],
                 本調臺羅=一句["臺羅"],
                 口語調臺羅=一句["口語調臺羅"],
 
-                sing5hong5舊編號=一句["舊編號"],
-                sing5hong5新編號=一句["新編號"],
-                sing5hong5有揀出來用無=一句["有揀出來無"],
+                sing5hong5舊編號=None,
+                sing5hong5新編號=None,
+                sing5hong5有揀出來用無=False,
                 愛先做無=self.判斷先愛先做無(一句["漢字"]),
             )
         with open(wav檔名, 'rb') as 檔案:
             音檔資料.原始檔.save(資料夾名 + '/' + 聲音檔名, File(檔案))
+    def 揣語者(self,語者資料,聽拍開始):
+        for 開始, _結束, 語者 in  語者資料:
+            if 開始>=聽拍開始:
+                return 語者
+            
