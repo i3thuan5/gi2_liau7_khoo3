2e77c784b0baae97c8c9308206e99f87cdd250cb
薛丞宏 <ihcaoe@gmail.com>
2017-12-08 14:19:05 +0800

    試驗嘛搬過來

--- 檢查工具/apps.py
+++ 檢查工具/apps.py
@@ -1,13 +1,10 @@
 from django.apps import AppConfig
 
-import 檢查工具.signals
-from 檢查工具 import signals
-
 
 class 檢查工具Config(AppConfig):
     name = '檢查工具'
 
     def ready(self):
         # registering signals
+        from 檢查工具 import signals
         signals
-        檢查工具.signals
