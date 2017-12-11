8e3adfa4052fd8acd293d8a78a488cfbb9cbc6f9
薛丞宏 <ihcaoe@gmail.com>
2017-12-11 20:05:24 +0800

    予na-phing會當重產生結果

--- 用字規範/admin.py
+++ 用字規範/admin.py
@@ -1,12 +1,23 @@
 
 from django.contrib import admin
 from 用字規範.models import 語料庫用字
+from 語料庫.models import 語料表
 
 
 class 語料庫用字管理(admin.ModelAdmin):
     list_display = ['id', '漢字', '羅馬字', '出處', '備註', ]
+    exclude = ['分詞']
     ordering = ['-id']
-    exclude=['分詞']
+    list_filter = ['id', '漢字', '羅馬字', ]
+
+    actions = ['自動檢查重做_愛五分鐘', ]
+
+    def 自動檢查重做_愛五分鐘(self, request, queryset):
+        self._重做()
+
+    def _重做(self):
+        for 語料 in 語料表.objects.all():
+            語料.save()
 
 
 admin.site.register(語料庫用字, 語料庫用字管理)
