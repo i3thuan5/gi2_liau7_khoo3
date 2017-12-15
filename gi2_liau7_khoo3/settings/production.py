df7acf9c002a3e9fbdb56c2a9d3078ac3958e24e
a8568730 <a8568730@hotmail.com>
2017-12-15 15:50:59 +0800

    Define STATIC_ROOT in production setting

--- gi2_liau7_khoo3/settings/production.py
+++ gi2_liau7_khoo3/settings/production.py
@@ -5,4 +5,6 @@ DEBUG = False
 # Application definition
 INSTALLED_APPS = SHARED_INSTALLED_APPS
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticAll')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
