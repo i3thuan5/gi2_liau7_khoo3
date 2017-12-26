5afdf7a18fe090e4fb1148fca31d8fa10e4dd1ee
薛丞宏 <ihcaoe@gmail.com>
2017-12-26 13:39:54 +0800

    用公家的函式

--- 語料庫/management/commands/LTS重算羅馬字漢字.py
+++ 語料庫/management/commands/LTS重算羅馬字漢字.py
@@ -1,9 +1,7 @@
-import Pyro4
 from django.db import transaction
 
 
 from 語料庫.management.commands.匯入1版TextGrid語料 import Command as textgrid指令
-from 程式.全漢全羅.原始通用處理 import 原始通用工具
 from 語料庫.models import 語料表
 
 
@@ -14,18 +12,12 @@ class Command(textgrid指令):
 
     @transaction.atomic()
     def handle(self, *args, **參數):
-        工具 = Pyro4.Proxy("PYRONAME:校對工具")
         for 語料 in (
             語料表.objects
             .filter(校對者__isnull=True)
             .filter(音檔__聲音檔名='LTS30.wav')
         ):
-            原始口語調臺羅 = (
-                原始通用工具.處理做口語調臺羅(語料.頭一版通用).看型('-', ' ')
-                .replace(' - ', '-')
-            )
-            漢字, 本調 = 工具.口語標漢字本調(原始口語調臺羅)
-            口語調臺羅 = self.照本調斷詞(本調, 原始口語調臺羅)
+            _原始口語調臺羅, 漢字, 本調, 口語調臺羅 = self.原始通用得著臺羅漢字(語料.頭一版通用)
             print(漢字, 本調, 口語調臺羅)
             語料.漢字 = 漢字
             語料.本調臺羅 = 本調
