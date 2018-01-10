094ca88dd04c21ac54f6a89daa3eb425a5ebcd68
薛丞宏 <ihcaoe@gmail.com>
2018-01-10 17:36:57 +0800

    匯出頭一版資料好做實驗

---
+++ 語料庫/management/commands/匯出1版語料.py
@@ -0,0 +1,45 @@
+
+import json
+from os.path import join, abspath
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+from 語料庫.models import 音檔表
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--輸出檔名',
+            type=str,
+            default='twisas1.json',
+        )
+
+    def handle(self, *args, **參數):
+        匯出資料 = []
+        for 音檔資料 in 音檔表.objects.order_by('id'):
+            語句 = []
+            for 資料 in (
+                音檔資料.資料
+                .filter(校對者__isnull=False)
+                .exclude(語料狀況__確定有校對=False)
+                .filter(語料狀況__Kaldi輸出=True)
+            ):
+                語句.append({
+                    "結束時間": 資料.聲音結束時間,
+                    "開始時間": 資料.聲音開始時間,
+                    "語者": 資料.語者,
+                    "漢字": 資料.漢字,
+                    '本調臺羅': 資料.本調臺羅,
+                    "口語臺羅": 資料.頭一版轉出來的口語調臺羅,
+                    "內容": 資料.頭一版轉出來的口語調臺羅,
+                })
+            匯出資料.append({
+                '影音所在': join(abspath(settings.MEDIA_ROOT), 音檔資料.原始檔.name),
+                '聽拍資料': 語句
+            })
+        with open(參數['輸出檔名'], 'w') as 檔案:
+            json.dump(匯出資料, 檔案, ensure_ascii=False, indent=2, sort_keys=True)
