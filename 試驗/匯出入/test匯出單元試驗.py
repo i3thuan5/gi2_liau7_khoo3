9625eab0a7a1118309e3b334922ecff23cb56dc2
薛丞宏 <ihcaoe@gmail.com>
2018-02-05 19:11:47 +0800

    頭一版資料輸出嘛改改咧

--- 試驗/匯出入/test匯出單元試驗.py
+++ 試驗/匯出入/test匯出單元試驗.py
@@ -35,6 +35,7 @@ class 匯出試驗(TestCase):
             漢字="阮小弟的個性乎，較直啦，較衝碰",
             本調臺羅="gun2 sio2-ti7 e5 ko3-sing3--honnh4, khah4 tit8--lah4, khah4 tshong2-pong7",
             口語調臺羅="gun1 sio1-ti7 e7 ko2-sing3 honn3, khah8 tit8 la3, khah8 tshong1-pong7",
+            頭一版轉出來的口語調臺羅="gun1 sio1-ti7 e7 ko2-sing3 honn3, khah8 tit8 la3, khah8 tshong1-pong7",
             sing5hong5舊編號='333',
             sing5hong5新編號='333',
             sing5hong5有揀出來用無=True,
@@ -45,12 +46,25 @@ class 匯出試驗(TestCase):
         self.一音檔 = self.新增音檔('戲劇', 'dirsui2', 'sui2.wav', 'sui2.txt',)
         self.一助理 = self.新增助理('小豬', 'a@gmail.com', '3333')
 
-    def test今仔日校對數量(self):
+    def test2版愛分詞(self):
         self.新增語料(self.一助理, self.一音檔)
         with TemporaryDirectory() as 資料夾:
             檔名 = join(資料夾, 'twisas.json')
             with io.StringIO() as 資訊:
                 call_command('匯出2版語料', '--輸出檔名', 檔名, stdout=資訊)
+            with open(檔名) as 檔案:
+                資料 = json.load(檔案)
+        self.assertEqual(
+            資料[0]["聽拍資料"][0]['內容'],
+            "gun1 sio1-ti7 e7 ko2-sing3 honn3 , khah8 tit8 la3 , khah8 tshong1-pong7"
+        )
+
+    def test1版愛分詞(self):
+        self.新增語料(self.一助理, self.一音檔)
+        with TemporaryDirectory() as 資料夾:
+            檔名 = join(資料夾, 'twisas.json')
+            with io.StringIO() as 資訊:
+                call_command('匯出1版語料', '--輸出檔名', 檔名, stdout=資訊)
             with open(檔名) as 檔案:
                 資料 = json.load(檔案)
         self.assertEqual(
